00f42257b06a30949c7f5ef6e68fac5e17da9303
Vladyslav-Frolov <tertey7@gmail.com>
2020-04-22 01:25:40 +0300

    Arrays

--- src/main/java/info/vladyslav/codeLibrary/basics/ArraysBasic.java
+++ src/main/java/info/vladyslav/codeLibrary/basics/ArraysBasic.java
@@ -4,11 +4,21 @@ import java.util.Arrays;
 
 public class ArraysBasic {
     public static void main(String[] args) {
+        // объявление переменной одномерного массива, все виды
+        int[] i; // ← эталонный способ
+        int b[];
+
+        // объявление переменной двумерного массива, все способы
+        int[][] c; // ← эталонный способ
+        int[] d[];
+        int e[][];
+
+
         // присвоение по умолчанию
         int[] arrayInt = new int[10]; // на печать выведет "0" (нули)
         float[] arrayFloat = new float[10]; // на печать выведет "0.0"
-        boolean [] arrayBool = new boolean[10]; // на печать выведет "false"
-        String [] arrayStirng = new String[10]; // на печать выведет "null"
+        boolean[] arrayBool = new boolean[10]; // на печать выведет "false"
+        String[] arrayStirng = new String[10]; // на печать выведет "null"
 
         System.out.println(Arrays.toString(arrayInt));
         System.out.println(Arrays.toString(arrayFloat));
