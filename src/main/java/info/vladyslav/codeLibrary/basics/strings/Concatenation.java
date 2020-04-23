31ce65d4be01ab49d2d93d107d92fa92d904211b
Vladyslav-Frolov <tertey7@gmail.com>
2020-04-23 04:37:56 +0300

    concatenation 2

--- src/main/java/info/vladyslav/codeLibrary/basics/strings/Concatenation.java
+++ src/main/java/info/vladyslav/codeLibrary/basics/strings/Concatenation.java
@@ -6,9 +6,16 @@ public class Concatenation {
         String b = 2 + 2 + " java " + 2 + 2; // 4 java 22
         String c = 2 + 2 + " java " + (2 + 2); // 4 java 4
         String d = (2 + 2) + " java"; // 4 java
+
+//        String e = 2 + 2; // выдаст ошибку, нужно конкатинировать со стрингом
+        int i = 2;
+        String is = "" + i; // 2
+
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
+        System.out.println(is);
+
     }
 }
