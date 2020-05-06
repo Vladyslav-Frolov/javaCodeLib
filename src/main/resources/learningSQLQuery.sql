944da44d05a23038ab89b84579dcbcc8caad720c
Vladyslav-Frolov <tertey7@gmail.com>
2020-05-06 22:28:01 +0300

    Learning SQL my query from book

--- src/main/resources/learningSQLQuery.sql
+++ src/main/resources/learningSQLQuery.sql
@@ -646,4 +646,129 @@ WHERE account_id = (SELECT MAX(account_id) FROM account);
 SELECT MAX(account_id)
 FROM account;
 
-DESC account
+DESC account;
+
+SELECT account_id, product_cd, cust_id, avail_balance
+FROM account
+WHERE open_emp_id <> (SELECT e.emp_id
+FROM employee e
+INNER JOIN branch b
+  ON e.assigned_branch_id = b.branch_id
+WHERE e.title = 'Head Teller'
+AND b.city = 'Woburn');
+
+# Subquery returns more than 1 row
+SELECT account_id, product_cd, cust_id, avail_balance
+FROM account
+WHERE open_emp_id <> (SELECT e.emp_id
+FROM employee e
+INNER JOIN branch b
+  ON e.assigned_branch_id = b.branch_id
+WHERE e.title = 'Teller'
+AND b.city = 'Woburn');
+
+SELECT branch_id, name, city
+FROM branch
+WHERE name IN ('Headquarters', 'Quincy Branch');
+
+SELECT branch_id, name, city
+FROM branch
+WHERE name = 'Headquarters'
+OR name = 'Quincy Branch'
+
+SELECT emp_id, fname, lname, title
+FROM employee
+WHERE emp_id IN (SELECT superior_emp_id
+FROM employee);
+
+
+SELECT emp_id, fname, lname, title
+FROM employee
+WHERE emp_id NOT IN (SELECT superior_emp_id
+FROM employee
+WHERE superior_emp_id IS NOT NULL);
+
+SELECT emp_id, fname, lname, title
+FROM employee
+WHERE emp_id <> ALL (SELECT superior_emp_id
+FROM employee
+WHERE superior_emp_id IS NOT NULL);
+
+# возвратиться пустой набор
+SELECT emp_id, fname, lname, title
+FROM employee
+WHERE emp_id NOT IN (1, 2, NULL);
+
+SELECT account_id, cust_id, product_cd, avail_balance
+FROM account
+WHERE avail_balance < ALL (SELECT a.avail_balance
+FROM account a
+INNER JOIN individual i
+  ON a.cust_id = i.cust_id
+WHERE i.fname = 'Frank'
+AND i.lname = 'Tucker');
+
+SELECT account_id, cust_id, product_cd, avail_balance
+FROM account
+WHERE avail_balance > ANY (SELECT a.avail_balance
+FROM account a
+INNER JOIN individual i
+  ON a.cust_id = i.cust_id
+WHERE i.fname = 'Frank'
+AND i.lname = 'Tucker');
+
+SELECT account_id, product_cd, cust_id
+FROM account
+WHERE open_branch_id = (SELECT branch_id
+FROM branch
+WHERE name = 'Woburn Branch')
+AND open_emp_id IN (SELECT emp_id
+FROM employee
+WHERE title = 'Teller'
+OR title = 'Head Teller');
+
+SELECT account_id, product_cd, cust_id
+FROM account
+WHERE (open_branch_id, open_emp_id) IN
+(SELECT b.branch_id, e.emp_id
+FROM branch b
+INNER JOIN employee e
+  ON b.branch_id = e.assigned_branch_id
+WHERE b.name = 'Woburn Branch'
+AND (e.title = 'Teller' OR e.title = 'Head Teller'));
+
+SELECT 'ALERT! : Account #1 Has Incorrect Balance!'
+FROM account
+WHERE (avail_balance, pending_balance) <>
+(SELECT SUM( < expression to generate available balance>), SUM(
+<expression to generate pending balance
+>
+)
+FROM transaction
+WHERE account_id = 1
+)
+AND
+account_id
+=
+1;
+
+SELECT c.cust_id, c.cust_type_cd, c.city
+FROM customer c
+WHERE 2 = (SELECT COUNT(*)
+FROM account a
+WHERE a.cust_id = c.cust_id
+group by c.cust_id); # без этого блока груп бай, код тоже выполнется корректно
+
+SELECT COUNT(*)
+FROM account a,
+customer c
+WHERE a.cust_id = c.cust_id
+group by c.cust_id;
+
+SELECT c.cust_id, c.cust_type_cd, c.city
+FROM customer c
+WHERE (SELECT SUM(a.avail_balance)
+FROM account a
+WHERE a.cust_id = c.cust_id)
+BETWEEN 5000 AND 10000;
+
