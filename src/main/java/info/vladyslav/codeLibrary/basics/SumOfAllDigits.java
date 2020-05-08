d114d48eb7125031aa8469bbe98268ad9762c190
Vladyslav-Frolov <tertey7@gmail.com>
2020-05-08 23:48:34 +0300

    The sum of all digits of a number

--- src/main/java/info/vladyslav/codeLibrary/basics/SumOfAllDigits.java
+++ src/main/java/info/vladyslav/codeLibrary/basics/SumOfAllDigits.java
@@ -3,13 +3,14 @@ package info.vladyslav.codeLibrary.basics;
 public class SumOfAllDigits {
     public static void main(String[] args) {
         int a = 555; // любое число
-        int suma = 0;
+        int suma = 0; // обязательная инициализация
 
         do {
             suma += a % 10;
             a /= 10; }
         while ((a % 10) != 0);
 
+        // вывод суммы
         System.out.println(suma);
     }
 }
