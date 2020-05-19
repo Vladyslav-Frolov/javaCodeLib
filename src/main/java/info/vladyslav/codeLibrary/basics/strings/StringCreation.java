29acd078253b7f14e4ee791c75c47186d514a819
Vladyslav-Frolov <tertey7@gmail.com>
2020-05-20 00:06:10 +0300

    Work with strings

--- src/main/java/info/vladyslav/codeLibrary/basics/strings/StringCreation.java
+++ src/main/java/info/vladyslav/codeLibrary/basics/strings/StringCreation.java
@@ -1,11 +1,21 @@
 package info.vladyslav.codeLibrary.basics.strings;
 
+import java.util.Arrays;
+
 public class StringCreation {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         sb.append("test");
         String s = String.valueOf(sb);
 
+        char [] ch = new char[]{'b', 'c', 'd', 'e', 'f', 'g'};
+        String s2 = String.valueOf(ch);
 
+        String [] str = new String[]{"b", "c", "d", "e", "f", "g"};
+        String s3 = String.valueOf(str); // хеш-код
+
+        System.out.println(s); // test
+        System.out.println(s2); // bcdefg
+        System.out.println(s3); // [Ljava.lang.String;@677327b6
     }
 }
