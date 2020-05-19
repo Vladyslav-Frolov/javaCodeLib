f2fe1445562ceda8724ad494e4542509cecb40dd
Vladyslav-Frolov <tertey7@gmail.com>
2020-05-19 23:48:54 +0300

    Work with strings

---
+++ src/main/java/info/vladyslav/codeLibrary/basics/strings/StringCreation.java
@@ -0,0 +1,11 @@
+package info.vladyslav.codeLibrary.basics.strings;
+
+public class StringCreation {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("test");
+        String s = String.valueOf(sb);
+
+
+    }
+}
