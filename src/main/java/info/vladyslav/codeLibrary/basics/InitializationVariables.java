ac55f1862b43bef1cb5a2bfd321e45f86c6cefed
Vladyslav-Frolov <tertey7@gmail.com>
2020-05-23 22:03:35 +0300

    Compilations tests

--- src/main/java/info/vladyslav/codeLibrary/basics/InitializationVariables.java
+++ src/main/java/info/vladyslav/codeLibrary/basics/InitializationVariables.java
@@ -1,5 +1,24 @@
 package info.vladyslav.codeLibrary.basics;
 
 public class InitializationVariables {
+    // нестатический контекст
+    // объявление имён
+    int a, b, c, d, e, f;
+
+    // будет ошибка компиляции,
+    // a = 10;
+    /**
+     * присвоить не в статическом контексте, если переменная уже объявлена,
+     * можно только через конструктор
+     */
+
+    // объявление с динамическим присвоением
+    int a1 = 1, b1 = 2, c1 = 3, d1 = 4;
+
+
+    public static void main(String[] args) {
+
+    }
+
 
 }
