12cfdb84137874753c8bb74fe8f0d3b002a28a39
Vladyslav-Frolov <tertey7@gmail.com>
2020-05-27 23:11:45 +0300

    sort

--- src/main/java/info/vladyslav/codeLibrary/basics/sort/InsertionSort.java
+++ src/main/java/info/vladyslav/codeLibrary/basics/sort/InsertionSort.java
@@ -28,32 +28,5 @@ public class InsertionSort {
         System.out.println(iteration); // можно удалять
         System.out.println(iteration2); // можно удалять
         System.out.println(iteration3); // можно удалять
-
-//------------------------------------------------------------
-        // 2-й способ, while снаружи (затранее, чем первый вариант)
-        boolean sorted = false;
-        int[] array = {5, 4, 3, 2, 1};
-        int temp1;
-        int iteration01 = 0; // можно удалять
-        int iteration02 = 0; // можно удалять
-        int iteration03 = 0; // можно удалять
-        while (!sorted) {
-            sorted = true;
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i] > array[i + 1]) {
-                    temp1 = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp1;
-                    sorted = false;
-                    iteration01++;
-                }
-                iteration02++;
-            }
-            iteration03++;
-        }
-        System.out.println(Arrays.toString(array));
-        System.out.println(iteration01); // можно удалять
-        System.out.println(iteration02); // можно удалять
-        System.out.println(iteration03); // можно удалять
     }
 }
