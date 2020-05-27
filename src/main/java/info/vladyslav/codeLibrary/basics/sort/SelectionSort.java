2829a0d35f62416e935a7a3a0ef1f96c5a23e0ea
Vladyslav-Frolov <tertey7@gmail.com>
2020-05-27 23:19:22 +0300

    sort

--- src/main/java/info/vladyslav/codeLibrary/basics/sort/SelectionSort.java
+++ src/main/java/info/vladyslav/codeLibrary/basics/sort/SelectionSort.java
@@ -6,17 +6,17 @@ public class SelectionSort {
         for (int i = 0; i < arr.length; i++) {
             int min = arr[i];//Предположительно минималтьный элемент
             int imin = i;//Предположительно индекс минимально элемента
-            for(int j = i + 1; j < arr.length; j++){
+            for (int j = i + 1; j < arr.length; j++) {
                 //Если находим минимум, то мы запоминаем его индекс
                 //И обновляем значение минимума
-                if(arr[j] < min){
-                    min  = arr[j];
+                if (arr[j] < min) {
+                    min = arr[j];
                     imin = j;
                 }
             }
             //Нашелся ли элемент который меньше чем тот который стоит на данной позиции.
             //Если нашелся, то меняем элементы местами
-            if( i != imin){
+            if (i != imin) {
                 int temp = arr[i];
                 arr[i] = arr[imin];
                 arr[imin] = temp;
