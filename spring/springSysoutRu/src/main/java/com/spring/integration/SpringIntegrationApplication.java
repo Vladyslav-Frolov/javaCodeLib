b0261e747ea0b1d582a19f8f1b5758638372de08
Vladyslav-Frolov <tertey7@gmail.com>
2020-12-15 02:32:33 +0200

    spring integration gateway sysout.ru

--- spring/springSysoutRu/src/main/java/com/spring/integration/SpringIntegrationApplication.java
+++ spring/springSysoutRu/src/main/java/com/spring/integration/SpringIntegrationApplication.java
@@ -1,16 +1,22 @@
 package com.spring.integration;
 
 import com.spring.integration.model.Animal;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan("com.spring.integration")
+@EnableIntegration
 public class SpringIntegrationApplication {
     @Bean
     DirectChannel outputChannel() {
@@ -27,25 +33,26 @@ public class SpringIntegrationApplication {
     // канал DirectChannel с именем animalFlow.input создается автоматически
     @Bean
     public IntegrationFlow animalFlow() {
-        return flow -> flow.handle("aService", "process")
+        return flow -> flow
+                .handle("aService", "process")
                 .handle("bService", "process")
                 .handle("cService", "process")
                 .channel("outputChannel");
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringIntegrationApplication.class, args);
-        ConfigurableApplicationContext ctx = SpringApplication.run(SpringIntegrationApplication.class, args);
+        final AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(SpringIntegrationApplication.class);
+        ctx.registerShutdownHook();
 
         DirectChannel outputChannel = ctx.getBean("outputChannel", DirectChannel.class);
         // обработчик внутри subscribe выполнится как только закончится выполнение flow
         outputChannel.subscribe(System.out::println);
         // запускаем выполнение flow
-        ctx.getBean(I.class).process(new Animal("cat"));
+//        ctx.getBean(I.class).process(new Animal("cat"));
 
         // можно было запустить flow отправкой сообщения во входной канал input:
-        // MessageChannel inputChannel = ctx.getBean("animalFlow.input", MessageChannel.class);
-        // inputChannel.send(MessageBuilder.withPayload(new Animal("cat")).build());
-//        ctx.close();
+         MessageChannel inputChannel = ctx.getBean("animalFlow.input", MessageChannel.class);
+         inputChannel.send(MessageBuilder.withPayload(new Animal("cat")).build());
+        ctx.close();
     }
 }
