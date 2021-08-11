0496d51e8cdc2a259c2e7026050efe68f6251aef
Vladyslav-Frolov <tertey7@gmail.com>
2021-08-11 14:06:36 +0300

    add static subpackage

---
+++ javaBasics/src/main/java/info/vladyslav/javabasics/u01core/core_01_base/bs25_static/Static.java
@@ -0,0 +1,8 @@
+package info.vladyslav.javabasics.u01core.core_01_base.bs25_static;
+
+import java.io.Serializable;
+
+public class Static implements Serializable {
+    private static final long serialVersionUID = 4308331351481082488L; // private for static
+
+}
