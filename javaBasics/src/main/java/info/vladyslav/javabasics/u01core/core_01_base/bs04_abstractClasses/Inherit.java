4e45b0aabb4e4aba228f9558c89c7db3b12e5403
VlaDySlaV <tertey7@gmail.com>
2024-06-07 18:03:03 +0300

    new tests for abstract files

---
+++ javaBasics/src/main/java/info/vladyslav/javabasics/u01core/core_01_base/bs04_abstractClasses/Inherit.java
@@ -0,0 +1,13 @@
+package info.vladyslav.javabasics.u01core.core_01_base.bs04_abstractClasses;
+
+public class Inherit extends InheritAbstract{
+    @Override
+    void test(Integer integer) {
+
+    }
+
+    @Override
+    void test() {
+
+    }
+}
---
+++ javaBasics/src/main/java/info/vladyslav/javabasics/u01core/core_01_base/bs04_abstractClasses/InheritAbstract.java
@@ -0,0 +1,6 @@
+package info.vladyslav.javabasics.u01core.core_01_base.bs04_abstractClasses;
+
+public abstract class InheritAbstract extends AbstractClass{
+
+    abstract void test(Integer integer);
+}
